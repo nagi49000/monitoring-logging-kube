eb7decddcf4e979c09e2ab8ed75322ca70993c91
nagi49000 <nagi49000>
2023-02-09 16:45:39 +0000

    added fluent logging to fastapi app

--- services/api/api/fastapi_app.py
+++ services/api/api/fastapi_app.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
-import datetime
 import logging
 import os
+from fastapi import FastAPI
+from datetime import datetime
 from pydantic import BaseModel
+from fluent.handler import (
+    FluentHandler,
+    FluentRecordFormatter
+)
 
 
 class HelloWorldResponse(BaseModel):
@@ -30,36 +34,58 @@ class ParrotBackResponse(BaseModel):
     results: ParrotBackResponseResults
 
 
-def get_gunicorn_logger(name="gunicorn.error"):
-    logger = logging.getLogger(name)  # dirty way of wiring into the gunicorn logger
-    # overwrite the root logger with the gunicorn logger
+def get_gunicorn_logger(name="gunicorn.error", app_name="my app"):
+    # dirty way of wiring into the gunicorn logger
+    logger = logging.getLogger(name)
+
+    # set up a Fluent handler for sending logs to fluent
+    fluent_host = os.getenv("FLUENT_HOST", "localhost")
+    fluent_port = int(os.getenv("FLUENT_PORT", "24224"))
+
+    fluent_format = {
+        "host": "%(hostname)s",
+        "where": "%(module)s.%(funcName)s",
+        "type": "%(levelname)s",
+        "stack_trace": "%(exc_text)s"
+    }
+    h = FluentHandler(app_name, host=fluent_host, port=fluent_port)
+    h.setFormatter(FluentRecordFormatter(fluent_format))
+    logger.addHandler(h)
+
+    # overwrite the root logger handlers with the gunicorn logger handlers,
+    # so that root logger messages get sent to gunicorn and fluent
     root_logger = logging.getLogger()
     root_logger.handlers = logger.handlers
     root_logger.setLevel(logger.level)
+    logger.info(f"Fluent logging to {fluent_host} on port {fluent_port}")
     return logger
 
 
 def create_app(logger_name="gunicorn.error"):
-    logger = get_gunicorn_logger(name=logger_name)
     app_name = os.getenv("APP_NAME", "simple test app")
     root_path = os.getenv("ROOT_PATH", None)
+    logger = get_gunicorn_logger(name=logger_name, app_name=app_name)
     app = FastAPI(title=app_name, root_path=root_path)
 
-    @app.get("/hello_world", response_model=HelloWorldResponse)
+    @ app.get("/hello_world", response_model=HelloWorldResponse)
     async def hello_world():
-        logger.debug('/hello_world')
+        logger.debug("/hello_world")
         return {"message": "Hello World"}
 
-    @app.post("/parrot_back", response_model=ParrotBackResponse)
+    @ app.post("/parrot_back", response_model=ParrotBackResponse)
     async def parrot_back(p: ParrotRequest):
         p_dict = p.dict()
-        logger.debug('/parrot_back: '+str(p_dict))
-        params = p_dict['parrot_request']
-        r_dict = {'header': p_dict['header'],
-                  'results': {'time': datetime.datetime.utcnow().isoformat(timespec='seconds')+'Z',
-                              'parrot': 'parrot back ' + params['sep'].join([params['parrot_str']]*params['n_repeat'])
-                              }
-                  }
+        logger.debug(f"/parrot_back: {p_dict}")
+        # get the params for making a parrot back message
+        q = p_dict["parrot_request"]
+        parrot_message = q["sep"].join([q["parrot_str"]] * q["n_repeat"])
+        r_dict = {
+            "header": p_dict["header"],
+            "results": {
+                "time": datetime.utcnow().isoformat(timespec="seconds") + "Z",
+                "parrot": f"parrot back {parrot_message}"
+            }
+        }
         return r_dict
 
     return app
